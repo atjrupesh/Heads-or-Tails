a9ffec0e373fe7d17ba41d7a6b2d214b7e33ba32
Atj Rupesh <62754636+atjrupesh@users.noreply.github.com>
2021-10-27 11:58:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+
+
+toss = random.randint(0, 1)
+
+t1 = toss
+
+if t1 == 1:
+ print("Heads")
+else:
+ print('Tails')
+
+
+
+
+
+
+
+
+
+
+
+
+
